c5160ee2971535d9cda6ebe6b298dd460458a7c3
DimaKuprikhin <dimakuprikhin@gmail.com>
2021-02-03 21:34:08 +0300

    Написаны комментарии для Presenter.

--- src/main/java/backend/Presenter.java
+++ src/main/java/backend/Presenter.java
@@ -8,8 +8,15 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.function.Predicate;
 
+/**
+ * Класс, реализующий представителя в паттерне MVP. Используется
+ * пользовательским для обращения к базе данных.
+ */
 public class Presenter {
 
+    /**
+     * Ссылка на базу контактов.
+     */
     @NotNull
     private final AbstractDatabase database;
 
@@ -18,10 +25,20 @@ public class Presenter {
                 " является налом.");
     }
 
+    /**
+     * Сохраняет изменения в базе.
+     * @throws IOException При ошибке записи изменений.
+     */
     public void saveDatabase() throws IOException {
         database.save();
     }
 
+    /**
+     * Получение коллекции контактов по запросу.
+     * @param predicate Предикат, указывающий, какие элементы необходимо
+     *                  вернуть.
+     * @return Unmodifiable коллекцию контактов, удовлетворяющих предикату.
+     */
     @NotNull
     public Collection<Contact> getByPredicate(
             @NotNull Predicate<Contact> predicate) {
@@ -30,6 +47,13 @@ public class Presenter {
         return database.getByPredicate(predicate);
     }
 
+    /**
+     * Добавляет контакт в базу. Если в базе уже есть дубликат этого контакта,
+     * то база не меняется.
+     * @param contact Добавляемый контакт.
+     * @return True, если этот контакт был добавлен, false, если такой контакт
+     * уже находился в базе.
+     */
     public boolean addContact(@NotNull Contact contact) {
         Objects.requireNonNull(contact, "Ссылка на добавляемый контакт" +
                 " является налом.");
@@ -42,15 +66,16 @@ public class Presenter {
         return true;
     }
 
+    /**
+     * Удаляет контакт из базы. При отсутствии удаляемого контакта в базе,
+     * состояние базы не меняется.
+     * @param contact Удаляемый контакт.
+     * @return True, если элемент был удален, false, если удаляемого элемента
+     * не было в базе.
+     */
     public boolean removeContact(@NotNull Contact contact) {
         Objects.requireNonNull(contact, "Ссылка на удаляемый контакт" +
                 " является налом.");
-        if(database.getByPredicate(
-                contactInContacts -> contactInContacts.equals(contact))
-                .size() != 1) {
-            return false;
-        }
-        database.remove(contact);
-        return true;
+        return database.remove(contact);
     }
 }
